27b6af013193efdd5364e927c367adbf593884db
RushilChalla <challa.rushil@gmail.com>
2026-09-13 18:01:36 -0400

    Add reporting reviews

--- movies/admin.py
+++ movies/admin.py
@@ -4,5 +4,9 @@ from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie', 'user', 'reported', 'date']
+    list_filter = ['reported', 'date']
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
